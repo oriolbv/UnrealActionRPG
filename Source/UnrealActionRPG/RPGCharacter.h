48d069b76bce9327b3e5e20cc2150dfa6661d0a0
Oriol Burgaya <oriolbv@gmail.com>
2020-07-17 18:42:04 +0200

    RPG Character added into project

---
+++ Source/UnrealActionRPG/RPGCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RPGCharacter.generated.h"
+
+UCLASS()
+class UNREALACTIONRPG_API ARPGCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true")) class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true")) class UCameraComponent* FollowCamera;
+
+public:
+	// Sets default values for this character's properties
+	ARPGCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
